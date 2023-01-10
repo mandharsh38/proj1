fdc3f192cdc107002442a1210fea264c157ed89d
pc119 <abhinav.technaitra@gmail.com>
2023-01-10 17:29:07 +0530

    update 530

--- index.php
+++ index.php
@@ -15,34 +15,34 @@
             <div class="row">
                 <div class="col-md-5">
                     <div class="form-floating">
-                        <input type="number" class="form-control" id="numMeasure" name="numMeasure" placeholder="numMeasure" required>
+                        <input type="number" class="form-control" id="numMeasure" name="numMeasure" placeholder="numMeasure" value="<?php echo $_POST['numMeasure']; ?>" required>
                         <label for="numMeasure">Enter no. of Measurements:</label>
                     </div>
                 </div>
                 <div class="col-md-5">
                     <div class="form-floating">
-                        <input type="number" class="form-control" id="cylCoeff" name="cylCoeff" placeholder="cylCoeff" required>
-                        <label for="cylCoeff">Cyl Coefficent c= </label>
+                        <input type="number" class="form-control" id="cylCoeff" name="cylCoeff" placeholder="cylCoeff" value="<?php echo $_POST['cylCoeff']; ?>" required>
+                        <label for="cylCoeff">Cyl Coefficent (c) </label>
                     </div>
                 </div>
-            </div>
+            </div><br>
             <div class="row">
                 <div class="col-md-3">
                     <div class="form-floating">
-                        <input type="number" class="form-control" id="cylCoeff" name="cylCoeff" placeholder="cylCoeff" required>
-                        <label for="cylCoeff">Enter Plate Thickness (mm) </label>
+                        <input type="number" class="form-control" id="plateThick" name="plateThick" placeholder="plateThick" value="<?php echo $_POST['plateThick']; ?>" required>
+                        <label for="plateThick">Enter Plate Thickness (mm) </label>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="form-floating">
-                        <input type="number" class="form-control" id="cylCoeff" name="cylCoeff" placeholder="cylCoeff" required>
-                        <label for="cylCoeff">Enter Plate Length (mm) </label>
+                        <input type="number" class="form-control" id="plateLen" name="plateLen" placeholder="plateLen" value="<?php echo $_POST['plateLen']; ?>" required>
+                        <label for="plateLen">Enter Plate Length (mm) </label>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="form-floating">
-                        <input type="number" class="form-control" id="cylCoeff" name="cylCoeff" placeholder="cylCoeff" required>
-                        <label for="cylCoeff">Enter Plate Width (mm) </label>
+                        <input type="number" class="form-control" id="plateWidth" name="plateWidth" placeholder="plateWidth" value="<?php echo $_POST['plateWidth']; ?>" required>
+                        <label for="plateWidth">Enter Plate Width (mm) </label>
                     </div>
                 </div>
                 <div class="col-md-2">
@@ -51,35 +51,37 @@
             </div>
         </form>
     </div>
-
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Measurement Number</th>
-                <th scope="col">Diameter (w) mils</th>
-                <th scope="col">Depth (d) mils</th>
-                <th scope="col">Depth % of Plate Thickness</th>
-                <th scope="col">w/d</th>
-                <th scope="col">Handle</th>
-                <th scope="col">Handle</th>
-                <th scope="col">Handle</th>
-                <th scope="col">Handle</th>
-                <th scope="col">Handle</th>
-                <th scope="col">Handle</th>
-            </tr>
-        </thead>
-        <tbody>
+    <br><br>
+    <div>
+        <table class="table table-responsive">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Diameter (w) mils</th>
+                    <th scope="col">Depth (d) mils</th>
+                    <th scope="col">Depth % of Plate Thickness</th>
+                    <th scope="col">w/d</th>
+                    <th scope="col">Estimate w/c (c=<?php echo $_POST['cylCoeff']; ?>)</th>
+                    <th scope="col">Volume Max</th>
+                    <th scope="col">Volume Min</th>
+                </tr>
+            </thead>
+            <tbody>
                 <?php for ($i=0; $i <$_POST['numMeasure'] ; $i++) { ?>
 
                 <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
+                    <th scope="row"><?php echo $i+1 ?></th>
+                    <td><input type="number"></td>
+                    <td><input type="number"></td>
+                    <td>test</td>
+                    <td>test</td>
+                    <td>test</td>
+                    <td>test</td>
+                    <td>test</td>
                 </tr>
-
                 <?php } ?>
             </tbody>
         </table>
+    </div>
 </body>
 </html>
