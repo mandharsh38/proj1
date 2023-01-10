059b294cf9bfd9dd00971f7fe0648fa367b614e4
pc119 <abhinav.technaitra@gmail.com>
2023-01-10 15:26:18 +0530

    update

--- index.php
+++ index.php
@@ -4,11 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <title>Home</title>
 </head>
 <body>
-        <form action="POST">
-            
-        </form>
+    <?php include 'header.php'?>
+    <form class="form-floating" action="" method="POST">
+        <label for="numMeasure">Enter no. of Measurements:</label>
+        <input type="text" class="form-control" id="numMeasure" name="numMeasure">
+        <label for="cylCoeff">Cyl Coefficent c= </label>
+        <input type="text" class="form-control" id="cylCoeff" name="cylCoeff">
+    </form>
+    
 </body>
 </html>
