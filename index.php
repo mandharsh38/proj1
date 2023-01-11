de9e10b178304ddb290470f45e692fb6b19069bc
pc119 <abhinav.technaitra@gmail.com>
2023-01-11 17:08:12 +0530

    finish main table

--- index.php
+++ index.php
@@ -7,42 +7,42 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-    <title>Home</title>
+    <title>Homepage</title>
     
 </head>
 <body>
     <a href="index.php">Home</a><br><br><br>
     <div class="container">
             <div class="row">
-                <div class="col-md-5">
+                <div class="col-md-6">
                     <div class="form-floating">
-                        <input type="number" class="form-control" id="numMeasure" name="numMeasure" placeholder="numMeasure" value="<?php echo $_POST['numMeasure']; ?>" required>
+                        <input type="number" class="form-control" id="numMeasure" name="numMeasure" placeholder="numMeasure" required>
                         <label for="numMeasure">Enter no. of Measurements</label>
                     </div>
                 </div>
-                <div class="col-md-5">
+                <div class="col-md-6">
                     <div class="form-floating">
-                        <input type="number" class="form-control" id="cylCoeff" name="cylCoeff" placeholder="cylCoeff" value="<?php echo $_POST['cylCoeff']; ?>" required>
+                        <input type="number" class="form-control" id="cylCoeff" name="cylCoeff" placeholder="cylCoeff" required>
                         <label for="cylCoeff">Cyl Coefficent (c) </label>
                     </div>
                 </div>
             </div><br>
             <div class="row">
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="form-floating">
-                        <input type="number" class="form-control" id="plateThick" name="plateThick" placeholder="plateThick" value="<?php echo $_POST['plateThick']; ?>" required>
+                        <input type="number" class="form-control" id="plateThick" name="plateThick" placeholder="plateThick" required>
                         <label for="plateThick">Enter Plate Thickness (mm) </label>
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="form-floating">
-                        <input type="number" class="form-control" id="plateLen" name="plateLen" placeholder="plateLen" value="<?php echo $_POST['plateLen']; ?>" required>
+                        <input type="number" class="form-control" id="plateLen" name="plateLen" placeholder="plateLen" required>
                         <label for="plateLen">Enter Plate Length (mm) </label>
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="form-floating">
-                        <input type="number" class="form-control" id="plateWidth" name="plateWidth" placeholder="plateWidth" value="<?php echo $_POST['plateWidth']; ?>" required>
+                        <input type="number" class="form-control" id="plateWidth" name="plateWidth" placeholder="plateWidth" required>
                         <label for="plateWidth">Enter Plate Width (mm) </label>
                     </div>
                 </div>
@@ -58,7 +58,7 @@
                     <th scope="col">Depth (d) mils</th>
                     <th scope="col">Depth % of Plate Thickness</th>
                     <th scope="col">w/d</th>
-                    <th scope="col">Estimate w/c (c=<?php echo $_POST['cylCoeff']; ?>)</th>
+                    <th scope="col">Estimate w/c</th>
                     <th scope="col">Volume Max</th>
                     <th scope="col">Volume Min</th>
                 </tr>
@@ -72,77 +72,83 @@
             
             var numRows = $(this).val();
             // console.log(numRows);            
-
-            var tableRowHtml = "<tr>"+
-                                    "<th scope='row'></th>" + 
-                                    "<td><input type='number' class='dia'></td>" + 
-                                    "<td><input type='number' class='depth'></td>" + 
-                                    "<td id='depthPercent' class='depthPercent'></td>" + 
-                                    "<td id='wByd' class='wByd'></td>" + 
-                                    "<td id='wByc' class='wByc'></td>" + 
-                                    "<td id='volMax' class='volMax'></td>" + 
-                                    "<td id='volMin' class='volMin'></td>" + 
-                                "</tr>"
-
+            
             $('#mainTableBody').children().remove();
-
+            
             for (var i=1; i<=numRows; i++){
-                // console.log(i);
+
+                var tableRowHtml = "<tr>"+
+                                        "<th scope='row'>" + i + "</th>" + 
+                                        "<td><input type='number' id='dia" + i + "' class='dia'></td>" + 
+                                        "<td><input type='number' id='depth" + i + "' class='depth'></td>" + 
+                                        "<td class='depthPercent' id='depthPercent" + i + "'></td>" + 
+                                        "<td class='wByd' id='wByd" + i + "'></td>" + 
+                                        "<td class='wByc' id='wByc" + i + "'></td>" + 
+                                        "<td class='volMax' id='volMax" + i + "'></td>" + 
+                                        "<td class='volMin' id='volMin" + i + "'></td>" + 
+                                    "</tr>";
+                
                 $('#mainTableBody').append(tableRowHtml);
-                // , function(){
-                    // var newId = $(this).attr('id') + i;
-                    // $(this).attr('id', newId);
-                // })
             }
         })
     </script>
     <script>
-        // claculation functions
+        // claculations
 
         // depth %
-        function depthPercent(){
-//
+        function depthPercent(depth){
+            var plateThick = $('#plateThick').val();
+            var value = (depth/plateThick)*100 ;
+            value = Math.round(value*10)/10;
+            return value;
         }
 
         // w/d
-        function wByd(depth, dia){
-            return dia/depth;
+        function wByd(dia, depth){
+            var value = dia/depth;
+            value = Math.round(value*100)/100;
+            return value;
         }
 
-    </script>
-    <script>
+        // w/c
+        function wByc(dia){
+            var cylCoeff = $('#cylCoeff').val();
+            var value = dia/cylCoeff;
+            value = Math.round(value*100)/100;
+            return value;
+        }
+
+        // Max Volume
+        function volMax(dia, depth){
+            var value = Math.PI * depth * ((dia/2) ** 2);
+            value = Math.round(value*10)/10;
+            return value;
+        }
+
+        // Min Volume
+        function volMin(index){
+            var value = $('#volMax' + index).text();
+            value = value/3;
+            value = Math.round(value*10)/10;
+            return value;
+        }
+
         $(document).on('keyup','input.depth', function(){
-            var depth = $(this).val();
-            var dia = $(this).parent().prev().children().val();
+
+            var index = $(this).attr('id');
+            index = index.slice(-1);
+
+            var dia = $('#dia' + index).val();
+            var depth = $('#depth' + index).val();
 
             // console.log("depth ="+depth);
             // console.log("dia ="+dia);
 
-            // calculate depth %
-            var depthPercent = dia/depth;
-            $(this).parent().siblings('.wByd').text(wByd);
-
-            // calculate w/d
-            var wByd = dia/depth;
-            $(this).parent().siblings('.wByd').text(wByd);
-
-            // calculate w/d
-            var wByd = dia/depth;
-            $(this).parent().siblings('.wByd').text(wByd);
-
-            // calculate w/d
-            var wByd = dia/depth;
-            $(this).parent().siblings('.wByd').text(wByd);
-
-            // calculate w/d
-            var wByd = dia/depth;
-            $(this).parent().siblings('.wByd').text(wByd);
-
-            // calculate w/d
-            var wByd = dia/depth;
-            $(this).parent().siblings('.wByd').text(wByd);
-
-            // $(this).siblings('.depthPercent').text;
+            $('#depthPercent' + index).text(depthPercent(depth) + "%");
+            $('#wByd' + index).text(wByd(dia, depth));
+            $('#wByc' + index).text(wByc(dia));
+            $('#volMax' + index).text(volMax(dia, depth));
+            $('#volMin' + index).text(volMin(index));
         })
     </script>
 </body>
